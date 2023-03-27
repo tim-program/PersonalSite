41afe046a34e4cd2fc60c6bca69711bc675232cd
Tim D <timothy.deutsch@yahoo.com>
2023-03-27 17:42:25 -0400

    Style and workflow improvements

--- ContentLoader.js
+++ ContentLoader.js
@@ -22,6 +22,17 @@ function AddHtmlTileToPage(ContentType,ListingName,ListingInfo){
     ProjectContainer.innerHTML += HtmlString;
 }
 
+function PopulatePageBar(LinkPrefix){
+    var listingClass = "LinkTile PageText";
+    var htmlLinkBar = "<a href=\""+LinkPrefix+"\\index.html\"><li class=\""+listingClass+"\">Timothy Deutsch</li></a>\n" +
+    "<a href=\""+LinkPrefix+"\\projects.html\"><li class=\""+listingClass+"\">Projects</li></a>\n" +
+    "<a href=\""+LinkPrefix+"\\blog.html\"><li class=\""+listingClass+"\">Blog</li></a>\n" +
+    "<a href=\""+LinkPrefix+"\\TimothyDeutschResumeRevising.pdf\"><li class=\""+listingClass+"\">Resume (revising)</li></a>\n";
+
+    var pageBarElement = document.getElementById("PageBar");
+    pageBarElement.innerHTML += htmlLinkBar;
+}
+
 /*
  <a href="./index.html">
                 <div class="ProjectTitleTile">
@@ -32,9 +43,9 @@ function AddHtmlTileToPage(ContentType,ListingName,ListingInfo){
 */
 function CreateHtmlStringFromDetails(ContentType,ListingName,Title,Description,Year){
     var htmlString = "<a href=\"./"+ContentType+"/"+ListingName+".html\">\n";
-    htmlString += "<div class = \"ProjectTitleTile\">\n";
-    htmlString += "<h1>" + Title + "</h1>\n";
-    htmlString += "<p>" + Description + "</p>\n";
+    htmlString += "<div class = \"ProjectTitleTile LinkTile\">\n";
+    htmlString += "<h1 class = \"PageText\">" + Title + "</h1>\n";
+    htmlString += "<p class = \"PageText\">" + Description + "</p>\n";
     htmlString += "</div>\n";
     htmlString += "</a>\n";
 
