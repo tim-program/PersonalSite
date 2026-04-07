08d58801e171a36eebe5e8578f749d44cdf65c7f
Tim D <timothy.deutsch@yahoo.com>
2026-04-07 13:45:15 -0400

    Mobile link bar styling

--- ContentLoader.js
+++ ContentLoader.js
@@ -77,7 +77,19 @@ function AddHtmlTileToPage(ContentType,ListingName,ListingInfo){
 
 function PopulatePageBar(LinkPrefix){
     var listingClass = "LinkTile PageText";
-    var htmlLinkBar = "<a href=\""+LinkPrefix+"/index.html\"><li>Home</li></a>\n"+
+    
+var htmlLinkBar =   "<li><span class=\"HideBarBtn\">Dir</span><button id=\"toggle-btn\"><img src=\""+LinkPrefix+"/88x31.gif\"/></button></li>\n"+
+                    "<li class=\"active\"><a href=\""+LinkPrefix+"/index.html\"><span>Home</span></a></li>\n"+
+                    "<li><a href=\""+LinkPrefix+"/aboutme.html\"><span>Me</span></a></li>\n"+
+                    "<li><a href=\""+LinkPrefix+"/blog.html\"><span>Blog</span></a></li>\n"+
+                    "<li><a href=\""+LinkPrefix+"/ai.html\"><span>AI</span></a></li>\n"+
+                    "<li><a href=\""+LinkPrefix+"/gamedev.html\"><span>GameDev</span></a></li>\n"+
+                    "<li><a href=\""+LinkPrefix+"/art.html\"><span>Art</span></a></li>\n"+
+                    "<li><a href=\""+LinkPrefix+"/contact.html\"><span>Contact</span></a></li>\n"+
+                    "<li><a href=\""+LinkPrefix+"/TimothyDeutschResume.pdf\"><span>Resume</span></a></li>\n";
+
+/*
+    var htmlLinkBar = "<a href=\""+LinkPrefix+"/index.html\"><li clas=\"active\">Home</li></a>\n"+
                     "<a href=\""+LinkPrefix+"/aboutme.html\"><li>Me</li></a>\n"+
                     "<a href=\""+LinkPrefix+"/blog.html\"><li>Blog</li></a>\n"+
                     "<a href=\""+LinkPrefix+"/ai.html\"><li>AI</li></a>\n"+
@@ -85,7 +97,7 @@ function PopulatePageBar(LinkPrefix){
                     "<a href=\""+LinkPrefix+"/art.html\"><li>Art</li></a>\n"+
                     "<a href=\""+LinkPrefix+"/contact.html\"><li>Contact</li></a>\n"+
                     "<a href=\""+LinkPrefix+"/TimothyDeutschResume.pdf\"><li>Resume</li></a>\n";
-
+*/
     var pageBarElement = document.getElementById("PageBar");
     pageBarElement.innerHTML += htmlLinkBar;
 }
